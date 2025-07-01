319c12f8377b1c8da927c95533e74e89eb552b7e
Richard Wrench <rwrench@gmail.com>
2025-07-01 17:03:59 -0500

    migrations for ratings

--- Movies.Api.Cqrs/Movies.Api.Cqrs.Infrastructure/Migrations/20250701220033_Ratings7125.Designer.cs
+++ Movies.Api.Cqrs/Movies.Api.Cqrs.Infrastructure/Migrations/20250701220033_Ratings7125.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Movies.Api.Cqrs.Infrastructure.Migrations
 {
     [DbContext(typeof(MoviesDbContext))]
-    partial class MoviesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250701220033_Ratings7125")]
+    partial class Ratings7125
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
