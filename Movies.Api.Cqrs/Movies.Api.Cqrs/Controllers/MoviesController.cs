f84f7e8069ad9823cd8773380022478a3cffc605
Richard Wrench <rwrench@gmail.com>
2025-06-30 19:08:09 -0500

    Add MoviesController and configure MediatR support
    - Introduced `AssemblyMarker` class for assembly scanning.
    - Created `MoviesController` with a POST endpoint for movie creation.
    - Updated project file to include MediatR package reference.
    - Cleaned up project structure by removing unnecessary folder group.
    - Configured Entity Framework Core with SQL Server in `Program.cs`.
    - Registered MediatR and scoped `IMovieCommandRepository` service.

---
+++ Movies.Api.Cqrs/Movies.Api.Cqrs/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Cqrs.Application.Commands;
+
+namespace Movies.Api.Cqrs.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MoviesController : Controller
+{
+    readonly IMediator _mediator;
+
+    public MoviesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        CreateMovieCommand command,
+        CancellationToken token)
+    {
+        var movieId = await _mediator.Send(command,token);
+        return Ok(movieId);
+    }
+
+}
--- Movies.Api.Cqrs/Movies.Api.Cqrs/Program.cs
+++ Movies.Api.Cqrs/Movies.Api.Cqrs/Program.cs
@@ -1,11 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Api.Cqrs.Application;
+using Movies.Api.Cqrs.Application.Repositories;
+using Movies.Api.Cqrs.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<MoviesDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Register MediatR 
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(AssemblyMarker).Assembly));
+builder.Services.AddScoped<IMovieCommandRepository, MovieCommandRepository>();
+
 
 var app = builder.Build();
 
